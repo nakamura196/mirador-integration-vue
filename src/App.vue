c16bac731e44cbd0831d2e706752cbb5060c4a27
nakamura196 <na.kamura.1263@gmail.com>
2021-05-12 06:25:14 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div id="demo"></div>
+  </div>
+</template>
+
+<script>
+import Mirador from 'mirador/dist/es/src/index';
+import { miradorImageToolsPlugin } from 'mirador-image-tools';
+
+export default {
+  name: 'App',
+  mounted(){
+    const config = {
+      id: 'demo',
+      windows: [{
+        imageToolsEnabled: true,
+        imageToolsOpen: true,
+        manifestId: 'https://purl.stanford.edu/sn904cj3429/iiif/manifest',
+      }],
+    };
+
+    Mirador.viewer(config, [
+      ...miradorImageToolsPlugin,
+    ]);
+  }
+}
+</script>
